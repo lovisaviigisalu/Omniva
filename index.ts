79346920599f0b5a83e7fd907099c92b4a52a6e2
leenalovisav <l.viigisalu@voco.ee>
2023-09-03 22:20:51 +0300

    front-endi lisamine ja

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import express, { Express, Request, Response } from "express";
 import automaat from "./controller/parcelmachines";
+import cors from "cors";
 
 const app: Express = express();
 
+app.use(cors());
 app.get('/', (req: Request, res: Response) => {
   res.send('It works!');
 });
---
+++ pakiautomaat-frontend/src/App.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [pakiautomaadid, setPakiautomaadid] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/parcelmachines")
+      .then(res => res.json())
+      .then(json => setPakiautomaadid(json));
+  }, []);
+
+  return (
+    <div className="App">
+      <select>
+        {pakiautomaadid.map(automaat => 
+            <option>
+                {automaat.NAME}
+            </option>)}
+      </select>
+    </div>
+  );
+}
+
+export default App;
+
